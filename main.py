95eb554779e8fd91534a92baa908708c3249fee1
Daviangelo <daviangelohuene2021@gmail.com>
2025-07-07 16:50:47 -0300

    aa

--- main.py
+++ main.py
@@ -74,7 +74,8 @@ st.markdown(f"""
 
 pagina = st.sidebar.selectbox("Escolha a página", ["Dashboard", "Tabela Completa"])
 
-API_URL = "http://192.168.0.214/inventario-api/api/v1/dash"
+API_URL = "https://www.centralretencao.com.br/inventario-api"
+
 try:
     response = requests.get(API_URL, headers={"accept": "application/json"})
     response.raise_for_status()
